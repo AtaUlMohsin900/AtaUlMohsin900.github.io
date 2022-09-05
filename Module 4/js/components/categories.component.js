4d1d430a8cad9f902547e02e2f494e8400ae8acf
AtaUlMohsin900 <103097405+AtaUlMohsin900@users.noreply.github.com>
2022-09-06 00:53:01 +0500

    updated

---
+++ Module 4/js/components/categories.component.js
@@ -0,0 +1,18 @@
+
+(function() {
+    "use strict";
+    
+    angular.module("MenuApp")
+      .component("categories", {
+        templateUrl: "js/components/categories.template.html",
+        controller: CategoriesComponentController,
+        bindings: {
+          items: "<"
+        }
+      });
+  
+    CategoriesComponentController.$inject = [];
+    function CategoriesComponentController() {
+      // var comp = this;
+    }
+  })();
